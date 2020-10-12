d92dd248305ee2ba04f20d98856b171609ddb8c9
jesusquezada <2016quezadaj@gmail.com>
2020-10-11 23:38:57 -0700

    Added boilerplate for ApexCharts

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-console.log('Connected...');
 // Chart Options
+const options = {
+    // chart: height, weight, color, type, background options
+    // series: This is the data of our chart
+};
 
 // Init Chart
-const chart;
+const chart = new ApexCharts(document.getElementById("#chart"), options);
 
 // Render Chart
+chart.render();
